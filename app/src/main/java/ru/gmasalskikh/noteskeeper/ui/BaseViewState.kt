9cf642632a83dead4eda70a25cd801f4432a33e1
gmasalskih <gmasalskih@mail.ru>
2020-08-09 15:39:39 +0300

    init commit

--- app/src/main/java/ru/gmasalskikh/noteskeeper/ui/BaseViewState.kt
+++ app/src/main/java/ru/gmasalskikh/noteskeeper/ui/BaseViewState.kt
@@ -6,6 +6,6 @@ abstract class BaseViewState<T>(data: T, err: Throwable?) {
     abstract val data: T
     abstract val err: Throwable?
     init {
-        if (data == null && err == null) throw IllegalStateException("Data and err property contains null")
+        if (data == null && err == null) throw IllegalStateException("Data and err property contains null. Only one of them can contains null, data or err property.")
     }
 }
